4a814ee98298a8c51c34cef08c753154f75f046a
Dow Drake <dowdrake@gmail.com>
2024-12-15 15:40:12 -0800

    Initial commit

---
+++ db_backup/backup.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+# Backup the live database locally (good to have it two places)
+
+# Remove old backups
+if [ -z "$BKPATH" ]
+then
+    echo "\$BKPATH is empty. Make sure the virtualenv is activated!"
+else
+    find $BKPATH -mtime +8 -exec rm {} \;
+    # Backup and gzip database
+    TODAY=`date -I`
+    FILEPATH=$BKPATH/dawn_$TODAY.gz
+    pg_dump -U $DB_USER  -h $DB_HOST -p $DB_PORT $DB_DATABASE | gzip > $FILEPATH
+fi
---
+++ db_backup/backup_live.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+# TODO: Backup the live database locally (good to have it two places)
+# (not working yet...)
+
+# Remove old backups
+if [ -z "$BKPATH_LIVE" ]
+then
+    echo "\$BKPATH_LIVE is empty. Make sure the virtualenv is activated!"
+else
+    find $BKPATH_LIVE -mtime +8 -exec rm {} \;
+    # Backup and gzip database
+    TODAY=`date -I`
+    FILEPATH=$BKPATH_LIVE/dawn_$TODAY.gz
+    PGPASSWORD=$DB_PASSWORD_LIVE pg_dump -U $DB_USER_LIVE \
+    -h $DB_HOST_LIVE -p $DB_PORT_LIVE $DB_DATABASE_LIVE | gzip > $FILEPATH
+fi
+
+
